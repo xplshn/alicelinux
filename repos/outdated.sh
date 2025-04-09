a64930636462bf24118ca7266f699ce2b696e821
emmett1 <emmett1.2miligrams@protonmail.com>
2025-04-10 07:26:30 +0800

    clang branch updated

--- repos/outdated.sh
+++ repos/outdated.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 fetchversion() {
-	curl -SsZA tmp https://repology.org/badge/latest-versions/$1.svg \
+	curl -s -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36" https://repology.org/badge/latest-versions/$1.svg \
 	| sed 's/, /,/g' \
 	| tr ' ' '\n' \
 	| grep -Eo 'central">.*<' \
